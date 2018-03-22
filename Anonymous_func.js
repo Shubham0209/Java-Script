5c19319e71ca9bba2abd6994175f9a25566415dd
Shubham0209 <shubhamp.singh@yahoo.com>
2018-03-22 18:35:45 +0530

    Create Anonymous_func.js

---
+++ Anonymous_func.js
@@ -0,0 +1,11 @@
+var laugh = function(num)/* finish the function expression */
+{
+    var hyu='';
+    for(var i=1;i<=num;i++)
+    {
+        hyu=hyu+'ha';
+    }
+    return hyu+'!';
+};
+
+console.log(laugh(10));
